54fd9498d85bad88bd0ece254ef17eca71d310fc
Venkatesh <rkamathvenkatesh@gmail.com>
2024-07-18 14:10:50 +0530

    configured server.

--- server/src/server.ts
+++ server/src/server.ts
@@ -12,7 +12,7 @@ const corsAnywhere = require('cors-anywhere');
 
 
 const host = process.env.HOST || '0.0.0.0';
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 
 corsAnywhere.createServer({
   originWhitelist: [], // Allow all origins
