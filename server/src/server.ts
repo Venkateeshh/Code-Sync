617e09e6fa7b8d1ee8c30dda06acc05c9e3994a7
Venkatesh <rkamathvenkatesh@gmail.com>
2024-07-18 14:14:02 +0530

    UPDATED VERCEL CONFIG

--- server/src/server.ts
+++ server/src/server.ts
@@ -12,7 +12,7 @@ const corsAnywhere = require('cors-anywhere');
 
 
 const host = process.env.HOST || '0.0.0.0';
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 corsAnywhere.createServer({
   originWhitelist: [], // Allow all origins
